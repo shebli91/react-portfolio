1a3975816a25a7c7c11062b1f04ccb8795d964a4
Mohammad Shebli <mhamadshebli@gmail.com>
2023-04-29 17:27:37 +0300

    adding the header component

---
+++ src/Components/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  return (
+    <header>
+      <a href="#" className="logo">
+        Mohammed Shebli
+      </a>
+      <nav className="navigation">
+        <a href="Shebli_CV.pdf" download>
+          CV
+        </a>
+        <a href="#software">Software</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
